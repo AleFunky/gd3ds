00496dd3f4360881cd3b22122e69db24b99604dd
advexed <advexed@gmail.com>
2026-04-02 18:41:49 +0200

    speed change particles + small particle colour fixes

--- source/graphics.c
+++ source/graphics.c
@@ -1079,6 +1079,12 @@ void draw_objects() {
 
         }
     }
+    if (!blend_enabled) change_blending(true);
+        drawParticleSystem(&slow_speed_particles, 0, 0, 1.f);
+        drawParticleSystem(&normal_speed_particles, 0, 0, 1.f);
+        drawParticleSystem(&fast_speed_particles, 0, 0, 1.f);
+        drawParticleSystem(&faster_speed_particles, 0, 0, 1.f);
+        change_blending(false);
 
     if (state.hitbox_display) {
         for (size_t s = 0; s < sprite_count; s++) {
