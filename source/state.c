e673af5fb7c7a871729ac851b1e8c2ec61395577
kandowontu <kandowontu@me.com>
2026-05-30 20:49:35 -0400

    Fix the pause thingy

--- source/state.c
+++ source/state.c
@@ -342,7 +342,7 @@ void init_variables() {
 
 void handle_death(Player *player) {
     play_sfx(&explode_sound, 1);
-    if (song_loaded) {
+    if (song_loaded && !playing_menu_loop) {
         pause_playback_mp3();
         seek_mp3(level_info.song_offset);
     }
