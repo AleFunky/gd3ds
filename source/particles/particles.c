a117bc23de63bea56dd1e645d96d817e6277e6ff
Melika Khan <70657892+zylonity@users.noreply.github.com>
2026-07-26 20:30:11 +0100

    Final tweaks for 3D
    Made extra particle effects stand out more, and made them move outwards in the correct orientation for the portals
    
    Added missing settings.txt file
    
    Removed is_citra from stereoscopic 3D settings

--- source/particles/particles.c
+++ source/particles/particles.c
@@ -420,6 +420,7 @@ void initParticleSystem(ParticleSystem* ps, const ParticleDefinition* cfg) {
     ps->emitterY = cfg->sourcePositiony;
     ps->scale = 1.0f;
     ps->depth = 0.0f;
+    ps->depthInwards = false;
     ps->posVarRotates = false;
     ps->affectedByMirror = true;
 
@@ -530,10 +531,10 @@ void drawParticleSystem(ParticleSystem* ps, float x_offset, float y_offset, floa
             y = (GSP_SCREEN_WIDTH - y);
         }
 
-        // Drift out of the screen as the particle ages
+        // Drift out of the screen as the particle ages, or back into it if inverted
         if (use_depth && d->totalTimeToLive[i] > 0) {
-            float age = 1.f - (d->timeToLive[i] / d->totalTimeToLive[i]);
-            x += get_depth_shift(ps->depth * age);
+            float life = d->timeToLive[i] / d->totalTimeToLive[i];
+            x += get_depth_shift(ps->depth * (ps->depthInwards ? life : 1.f - life));
         }
 
         C2D_ImageTint tint = { 0 };
