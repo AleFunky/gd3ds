1f99b35c0ac1e4bbac17d3ead5eee6446b35437b
Cloud54 <161091467+Cloud5474@users.noreply.github.com>
2026-08-01 14:28:58 -0400

    Evil UI stuff (#130)
    * random ui stuff
    
    invisible buttons are real and ui tweaks
    
    * made length text into buttons
    
    w
    
    * remove action area
    
    buttons can be invisible now
    
    * fix stupid thing

--- source/menus/components/ui_action_area.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "menus/core/ui_element.h"
-#include "menus/core/ui_screen.h"
-
-UIActionArea *ui_create_action_area(const UIContext *ctx);
-UIElement *ui_create_action_area_from_props(const UIContext *ctx, const UIPropertyList *props);
