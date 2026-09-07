98f5fdd30f85233e1026020eb6b167ad857deb5d
AleFunky <alefunkyextra@gmail.com>
2026-09-07 11:53:01 +0200

    Validate comment colors

--- source/menus/online_level_comments.c
+++ source/menus/online_level_comments.c
@@ -141,9 +141,19 @@ void populate_comments() {
                 
                 user_icon->glow = comment_entries[i].glow;
                 ui_icon_set_gamemode_index(user_icon, iconType, iconIndex);
-                ui_icon_set_glow(user_icon, colors[gd_to_gd3ds_color_table[comment_entries[i].glowCol]]);
-                ui_icon_set_p1(user_icon, colors[gd_to_gd3ds_color_table[comment_entries[i].col1]]);
-                ui_icon_set_p2(user_icon, colors[gd_to_gd3ds_color_table[comment_entries[i].col2]]);
+
+                int glow_col = comment_entries[i].glowCol;
+                if (glow_col < 0 || glow_col >= NUM_COLORS) glow_col = 12;
+                
+                int p1_col = comment_entries[i].col1;
+                if (p1_col < 0 || p1_col >= NUM_COLORS) p1_col = 12;
+                
+                int p2_col = comment_entries[i].col2;
+                if (p2_col < 0 || p2_col >= NUM_COLORS) p2_col = 12;
+
+                ui_icon_set_glow(user_icon, colors[gd_to_gd3ds_color_table[glow_col]]);
+                ui_icon_set_p1(user_icon, colors[gd_to_gd3ds_color_table[p1_col]]);
+                ui_icon_set_p2(user_icon, colors[gd_to_gd3ds_color_table[p2_col]]);
                 
                 ui_element_set_position((UIElement *)user_icon, -list_width + 20, -list_height + 15);
                 ui_element_set_scale((UIElement *)user_icon, 0.5f);
